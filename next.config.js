33689fc453c247d072dba08dace4a33f35943166
haneulchoistudo <haneulchoi.business@gmail.com>
2023-11-27 01:35:42 -0800

    fix: devSizes not iterable, added 'domain' property to images next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: [
-    "lh3.googleusercontent.com",
-    // {
-    //   loader: "cloudinary",
-    //   path: "https://res.cloudinary.com/haneulchoistudio/image/static",
-    // },
-  ],
+  images: { domains: ["lh3.googleusercontent.com"] },
 };
 
 module.exports = nextConfig;
